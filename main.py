f8d5adbf0f12108e0b7b39ca1713c9a70e180639
shabashik-tech <kushner.git@gmail.com>
2021-01-15 16:45:48 +0300

    прописал структуру html

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # https://www.youtube.com/watch?v=Gon0MvppfF8&t=245s
 
 import pyowm
+import eel
 
-city = 'Moscow, Russia'
-owm = pyowm.OWM('59ff4e66ae7a38fcb9a7a637165a4172')
-manager = owm.weather_manager()
+eel.init('web')
+eel.start('main.html', size=(300, 300))
 
-observation = manager.weather_at_place(city)
-weather = observation.weather
-
-temp = weather.temperature('celsius')['temp']
-
-print(f'В городе {city} сейчас {str(round(temp, 2))} градусов.')
+# city = 'Moscow, Russia'
+# owm = pyowm.OWM('59ff4e66ae7a38fcb9a7a637165a4172')
+# manager = owm.weather_manager()
+#
+# observation = manager.weather_at_place(city)
+# weather = observation.weather
+#
+# temp = weather.temperature('celsius')['temp']
+#
+# print(f'В городе {city} сейчас {str(round(temp, 2))} градусов.')
